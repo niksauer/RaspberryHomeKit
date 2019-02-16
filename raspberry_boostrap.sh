0c370c3aa77d121f387d66f982f16d4e11673c63
Niklas Sauer <nik.sauer@me.com>
2019-02-16 15:01:09 +0100

    add shell script with necessary steps on setting up new raspberry pi 3 for this project

---
+++ raspberry_boostrap.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# HEADLESS SETUP
+# https://www.raspberrypi.org/documentation/installation/installing-images/README.md
+# https://raspberrypi.stackexchange.com/questions/10251/prepare-sd-card-for-wifi-on-headless-pi/57023#57023
+
+# LOCALE
+sudo nano /etc/locale.gen # uncomment en_US.UTF-8
+sudo locale-gen en_US.UTF-8
+sudo nano /etc/default/locale # set LANG=en_US.UTF-8
+sudo update-locale en_US.UTF-8
+exit
+
+# WELCOME MESSAGE
+sudo vi /etc/motd # remove content as desired
+
+# UTILITIES
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install git
+
+# OH MY ZSH
+# https://github.com/robbyrussell/oh-my-zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+
+# DOCKER
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo reboot
+
+# DOCKER COMPOSE
+sudo apt install python3-pip python3
+sudo pip3 install docker-compose
+
+# HOMEBRIDGE
+# https://github.com/oznu/docker-homebridge/wiki/Homebridge-on-Raspberry-Pi
+cd ~
+git clone https://github.com/niksauer/RaspberryHomeKit.git
+cd RaspberryHomeKit 
+docker-compose up -d 
+docker-compose logs -f # hostname -I > http://admin:admin:<ip>:8080 > change password, install plugins and update config as desired
